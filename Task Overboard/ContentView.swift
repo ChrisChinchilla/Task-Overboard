b8d2d074434ed1f91b15470938f196c03e29c6e2
Chris Chinchilla <chris@chrischinchilla.com>
2023-12-30 20:08:53 +0100

    Draft

---
+++ Task Overboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Task Overboard
+//
+//  Created by Chris Ward on 30.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Task Overboard/Task_OverboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_OverboardApp.swift
+//  Task Overboard
+//
+//  Created by Chris Ward on 30.12.23.
+//
+
+import SwiftUI
+
+@main
+struct Task_OverboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
